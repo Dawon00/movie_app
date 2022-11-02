f46343c226835cbea8612406dec6d95bb15920a2
Dawon00 <da6921@naver.com>
2022-11-03 04:46:00 +0900

    ADD: Detail.js, Home.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
-import Movie from "./Movie";
+import Movie from "./components/Movie";
+import ProTypes from "prop-types";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false); //로딩이 끝남.
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        //로딩중이 아닐 때 보여줄 화면
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              medium_cover_image={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            ></Movie>
-          ))}
-        </div>
-      )}{" "}
-    </div>
-  );
+  return null;
 }
+
+Movie.prototype = {
+  medium_cover_image: ProTypes.string.isRequired,
+  title: ProTypes.string.isRequired,
+  summary: ProTypes.string.isRequired,
+  genres: ProTypes.arrayOf(ProTypes.string).isRequired,
+};
 
 export default App;
